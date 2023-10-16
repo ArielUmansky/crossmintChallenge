9e406dc8aece25e31b14f9a66b502a971e7fec61
Ariel Umansky <umanskyariel@gmail.com>
2023-10-16 18:06:13 -0300

    Add error management and delete unessary code

--- src/megaverseApi/megaverseApi.js
+++ src/megaverseApi/megaverseApi.js
@@ -30,69 +30,25 @@ export class MegaverseApi {
                 if(response && response.data && response.data.goal) {
                     return response.data.goal
                 } else {
-                    return {...response, errorMessage: 'Unexpected response structure from Metaverse API'}
+                    throw new Error({...response, errorMessage: 'Unexpected response structure from Metaverse API while getting the goal'})
                 }
-            })
-            .catch((error) => {
-                return {...error, errorMessage: 'There was an error while requesting the goal from Metaverse API'}
             })
     }
 
     createPolyanet(row, column) {
         return axios.post(`${this.HOST_URL}/polyanets`, {row, column, candidateId: this.candidateId}, {})
-            .then((response) => {
-                if(response) {
-                    return response
-                } else {
-                    return {...response, errorMessage: 'Unexpected response structure from Metaverse API'}
-                }
-            })
-            .catch((error) => {
-                return {...error, errorMessage: 'There was an error while setting a polyanet in Metaverse API'}
-            })
     }
 
     createSaloon(row, column, color) {
         return axios.post(`${this.HOST_URL}/soloons`, {color, row, column, candidateId: this.candidateId}, {})
-            .then((response) => {
-                if(response) {
-                    return response
-                } else {
-                    console.error()
-                    return {...response, errorMessage: 'Unexpected response structure from Metaverse API'}
-                }
-            })
-            .catch((error) => {
-                return {...error, errorMessage: 'There was an error while setting a saloon in Metaverse API'}
-            })
     }
 
     createCometh(row, column, direction) {
         return axios.post(`${this.HOST_URL}/comeths`, {direction, row, column, candidateId: this.candidateId}, {})
-            .then((response) => {
-                if(response) {
-                    return response
-                } else {
-                    return {...response, errorMessage: 'Unexpected response structure from Metaverse API'}
-                }
-            })
-            .catch((error) => {
-                return {...error, errorMessage: 'There was an error while setting a cometh in Metaverse API'}
-            })
     }
 
     deletePolyanet(row, column) {
         return axios.delete(`${this.HOST_URL}/polyanets`, {data: {row, column, candidateId: this.candidateId}})
-            .then((response) => {
-                if(response) {
-                    return response
-                } else {
-                    return {...response, errorMessage: 'Unexpected response structure from Metaverse API'}
-                }
-            })
-            .catch((error) => {
-                return {...error, errorMessage: 'There was an error while deleting a polyanet in Metaverse API'}
-            })
     }
 
     getMap() {
@@ -101,23 +57,23 @@ export class MegaverseApi {
                 if(response && response.data && response.data.map) {
                     return response.data.map
                 } else {
-                    return {...response, errorMessage: 'Unexpected response structure from Metaverse API'}
+                    throw new Error({...response, errorMessage: 'Unexpected response structure from Metaverse API while getting the map'})
                 }
-            })
-            .catch((error) => {
-                return {...error, errorMessage: 'There was an error while requesting the map from Metaverse API'}
             })
     }
 
     async cleanMap() {
-        const map = (await this.getMap()).content
-        return map.forEach((row, rowIndex) => {
-            return row.forEach(async (col, colIndex) => {
-                await this.deletePolyanet(rowIndex, colIndex)
+        try {
+            const map = (await this.getMap()).content
+            return map.forEach((row, rowIndex) => {
+                return row.forEach(async (col, colIndex) => {
+                    await this.deletePolyanet(rowIndex, colIndex)
+                })
             })
-        })
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
     }
-
-
 
 }
--- src/megaverseChallenge.js
+++ src/megaverseChallenge.js
@@ -1,6 +1,8 @@
 import {MEGAVERSE_CODE_NAMES, MEGAVERSE_CODE_TYPES} from "./megaverseApi/megaverseApi";
 import {MOCKED_MEGAVERSE_CODE_TYPES} from "./megaverseApi/mockedMegaverseApi/mockedMegaverseApi";
 
+const REQUEST_DELAY_MS = 5000
+
 export class MegaverseChallenge {
 
     constructor(megaverseApi) {
@@ -13,48 +15,62 @@ export class MegaverseChallenge {
 
         let delay = 0
 
-        goal.forEach((row, rowIndex) => {
-            row.forEach(async (value, columnIndex) => {
-                delay++
+        try {
+            goal.forEach((row, rowIndex) => {
+                row.forEach(async (value, columnIndex) => {
 
-                //Thw following line is just a simple optimization
-                if(value === MEGAVERSE_CODE_NAMES.SPACE) return
+                    //Thw following line is just a simple optimization
+                    if(value === MEGAVERSE_CODE_NAMES.SPACE) return
 
-                // This delay mechanism is a workaround for the "too many request" 429 status errors thrown by the api
-                //setTimeout(async () => {
-                    if(value === MEGAVERSE_CODE_NAMES.POLYANET) {
-                        createOperations.push(this.megaverseApi.createPolyanet(rowIndex, columnIndex))
-                    }
+                    // This delay mechanism is a workaround for the "too many request" 429 status errors thrown by the api
+                    // It's commented to allow unit test to run green from the start
+                    //delay++
+                    //setTimeout(async () => {
+                        if(value === MEGAVERSE_CODE_NAMES.POLYANET) {
+                            createOperations.push(this.megaverseApi.createPolyanet(rowIndex, columnIndex))
+                        }
 
-                    const additionalParam = value.split('_')[0].toLowerCase()
+                        const additionalParam = value.split('_')[0].toLowerCase()
 
-                    if(value.includes(MEGAVERSE_CODE_NAMES.SALOON)) {
-                        createOperations.push(this.megaverseApi.createSaloon(rowIndex, columnIndex, additionalParam))
-                    }
+                        if(value.includes(MEGAVERSE_CODE_NAMES.SALOON)) {
+                            createOperations.push(this.megaverseApi.createSaloon(rowIndex, columnIndex, additionalParam))
+                        }
 
-                    if(goal[rowIndex][columnIndex].includes(MEGAVERSE_CODE_NAMES.COMETH)) {
-                        createOperations.push(this.megaverseApi.createCometh(rowIndex, columnIndex, additionalParam))
-                    }
-                //}, delay * 0)
+                        if(goal[rowIndex][columnIndex].includes(MEGAVERSE_CODE_NAMES.COMETH)) {
+                            createOperations.push(this.megaverseApi.createCometh(rowIndex, columnIndex, additionalParam))
+                        }
+                    //}, delay * REQUEST_DELAY_MS)
 
+                })
             })
-        })
 
-        return Promise.all(createOperations)
+            return Promise.all(createOperations)
+
+        } catch (error) {
+            // This loses the stacktrace. In newer versions there's a "cause" alternative to better wrap exceptions.
+            // See: https://stackoverflow.com/questions/42754270/re-throwing-exception-in-nodejs-and-not-losing-stack-trace
+            console.error(error)
+            throw new Error('There was an error while completing the challenge')
+        }
     }
 
     // This method runs over mocked code values because I wasn't able to check the actual ones before finishing the challenge
     async passesGoal() {
-        const map = (await this.megaverseApi.getMap()).content
-        const goal = await this.megaverseApi.getGoal()
-        return goal.every((row, rowIndex) => {
-            return row.every((value, columnIndex) => {
-                return (value === MEGAVERSE_CODE_NAMES.SPACE && map[rowIndex][columnIndex] === null)
-                    || (value === MEGAVERSE_CODE_NAMES.POLYANET && map[rowIndex][columnIndex] && map[rowIndex][columnIndex].type === MEGAVERSE_CODE_TYPES.POLYANET)
-                    || (value.includes(MEGAVERSE_CODE_NAMES.COMETH) && map[rowIndex][columnIndex] && map[rowIndex][columnIndex].type === MOCKED_MEGAVERSE_CODE_TYPES.COMETH && map[rowIndex][columnIndex].direction === value.split('_')[0].toLowerCase())
-                    || (value.includes(MEGAVERSE_CODE_NAMES.SALOON) && map[rowIndex][columnIndex] && map[rowIndex][columnIndex].type === MOCKED_MEGAVERSE_CODE_TYPES.SALOON && map[rowIndex][columnIndex].color === value.split('_')[0].toLowerCase())
+        try {
+            const map = (await this.megaverseApi.getMap()).content
+            const goal = await this.megaverseApi.getGoal()
+            return goal.every((row, rowIndex) => {
+                return row.every((value, columnIndex) => {
+                    return (value === MEGAVERSE_CODE_NAMES.SPACE && map[rowIndex][columnIndex] === null)
+                        || (value === MEGAVERSE_CODE_NAMES.POLYANET && map[rowIndex][columnIndex] && map[rowIndex][columnIndex].type === MEGAVERSE_CODE_TYPES.POLYANET)
+                        || (value.includes(MEGAVERSE_CODE_NAMES.COMETH) && map[rowIndex][columnIndex] && map[rowIndex][columnIndex].type === MOCKED_MEGAVERSE_CODE_TYPES.COMETH && map[rowIndex][columnIndex].direction === value.split('_')[0].toLowerCase())
+                        || (value.includes(MEGAVERSE_CODE_NAMES.SALOON) && map[rowIndex][columnIndex] && map[rowIndex][columnIndex].type === MOCKED_MEGAVERSE_CODE_TYPES.SALOON && map[rowIndex][columnIndex].color === value.split('_')[0].toLowerCase())
+                })
             })
-        })
+        } catch(error) {
+            console.error(error)
+            throw new Error('There was a problem while validating the current goal')
+        }
     }
 
 
